56288a43e57fe58556d2996595caecc67518e6bb
Mahesh Venkat <maheshvenkat@digienv.com>
2018-08-03 21:40:47 -0400

    Adding the initial project structure

---
+++ app/src/main/java/com/backbase/citylocator/CityLocatorActivity.java
@@ -0,0 +1,13 @@
+package com.backbase.citylocator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CityLocatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_locator);
+    }
+}
